a7e952e0d93965f0488a8381dc02357868f8ec27
Junaiyo <junaiyoreidelass@gmail.com>
2026-06-09 11:31:48 +0000

    some changes

--- src/pages/changelogs.jsx
+++ src/pages/changelogs.jsx
@@ -6,9 +6,9 @@ export const ChangeLogs = () => {
   const navigate = useNavigate();
   const versions = ["V1", "V1.5", "V2", "V3", "V3.5"];
   const vLogs = [["versão inicial"], ["sistema de rebirth", "change logs"],
-["sistema de jogos", "sistema de vender armaduras", "sistema de visibilidade de armaduras equipadas", "save automático (a cada ação importante)", "limite de inventário (expansível)"],
-["modo de jogo aventura, com 4 ilhas e 16 monstros","modo de jogo adivinha", "inventário expansível", "cooldown para desequipar, vender e melhorar armaduras", "interface levemente melhorada"], ["sistema de buffs temporários e permanentes", "menu de extras", "caminho /reset", "grande bug fix"]]
-  const [number, setNumber] = useState(0);
+["sistema de jogos", "sistema de vender armaduras", "sistema de visibilidade de armaduras equipadas", "save automático (a cada 10 minutos ou a cada ação importante)", "limite de inventário (expansível futuramente)"],
+["modo de jogo aventura, com 4 ilhas e 16 monstros","modo de jogo adivinha", "inventário expansível", "cooldown para desequipar, vender e melhorar armaduras", "interface levemente melhorada", "removido save a cada 10 minutos, agora só em ações importantes"], ["sistema de buffs temporários e permanentes", "menu de extras", "caminho /reset", "suporte para teclas em partes especificas", "grande bug fix"]]
+  const [number, setNumber] = useState(versions.length-1);
   
   return (
     <div className="logs">
@@ -17,7 +17,7 @@ export const ChangeLogs = () => {
         <h2>{versions[number]}</h2>
         {vLogs[number].map((log, index) => (
       <ul key={index}>
-        <li className="msgLog">Adicionado {log}</li>
+        <li className="msgLog">{log}</li>
       </ul>
         ))}
         
